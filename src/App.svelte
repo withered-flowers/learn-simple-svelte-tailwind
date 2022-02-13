992e735fbb3c3be99bb286387596611ec64bff26
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-02-13 13:30:16 +0000

    feat: add FirstComponent.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,14 @@
 <script>
+  // import the component here
+  import FirstComponent from "./lib/FirstComponent.svelte";
 </script>
 
 <div class="custom-container bg-slate-200">
   <p class="h1">Simple Svelte Apps with Tailwind</p>
+  <!-- Create new section to hold FirstComponent -->
+  <section>
+    <FirstComponent />
+  </section>
 </div>
 
 <style></style>
---
+++ src/lib/FirstComponent.svelte
@@ -0,0 +1,35 @@
+<script>
+  // every value declare will be reactive in svelte
+  // svelte reactivity is by assignments
+  let count = 0;
+
+  // method for set count state
+  const addCount = () => {
+    count++;
+  };
+
+  // method for reset count state
+  const resetCount = () => {
+    count = 0;
+  };
+</script>
+
+<div>
+  <h2 class="h2">First Component</h2>
+  <h3>
+    <!-- conditional rendering -->
+    {#if count === 0}
+      Don't you dare to click me !
+    {:else}
+      How dare you click me {count} time{count > 1 ? "s" : ""}
+    {/if}
+  </h3>
+  <p>
+    <!-- bind event on click with addCount -->
+    <button class="btn" on:click={addCount}>Dare-to-click</button>
+  </p>
+  <p>
+    <!-- bind event on click with resetCount -->
+    <button class="btn" on:click={resetCount}>Forget-me-not</button>
+  </p>
+</div>
